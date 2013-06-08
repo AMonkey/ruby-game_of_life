3564a54de8e5729cdc1527ac6972aed607b3151e
Will Jackson <greenerside71@gmail.com>
2013-06-07 17:32:26 -0700

    Fixed print function

--- gameoflife.rb
+++ gameoflife.rb
@@ -1,112 +1,115 @@
 #!/usr/bin/ruby
+module GameOfLife
+    class Board
+        def initialize(s)
+            @size = s
 
-class Board
-    def initialize(s)
-        @size = s
+            # Makes an array of arrays, each corresponding to a row, filled with dead cells
+            @board = Array.new(@size, Array.new(@size, Cell.new))
 
-        # Makes an array of arrays, each corresponding to a row, filled with dead cells
-        @board = Array.new(size, Array.new(size, Cell.new))
+        end
 
+        def populate_random(n = @size/2)
+            n.times { @board[Random.rand(@size)][Random.rand(@size)].birth! }
+
+        end
+
+        def print
+            s = ''
+            @board.each do |r|
+                # Top off each row
+                s << "-" * (@size * 2 + 1)
+                s << "\n|"
+                r.each do |c|
+                    if c.is_alive?
+                        s << 'x|'
+
+                    else
+                        s << ' |'
+
+                    end
+                end
+                s << "\n"
+
+            end
+
+            # Cap it and print
+            s << "-" * (@size * 2 + 1)
+            puts s
+
+        end
+
+        # Returns a flattened list of all neighbors of a given location
+        # Location must be a 2-member list of coordinates [row, coloumn]
+        def get_neighborhood(*location)
+            t = @board[location[0]-1].slice(location[1]-1, 3)
+            m = @board[location[0]].slice(location[1]-1, 3)
+            m.delete_at(1)
+            b = @board[location[0]+1].slice(location[1]-1, 3)
+
+            return [t,m,b].flatten
+
+        end
     end
 
-    def populate_random(n = @size/2)
-        n.times { @board[Random.rand(@size)][Random.rand(@size)].birth }
+    class Cell
+        def initialize
+            @current_state = false
+            @next_state = false
 
-    end
+        end
 
-    def print
-        s = ''
-        @board.each do |r|
-            # Top off each row
-            s += "-" * (@size * 2 + 1)
-            s += "\n"
-            r.each do |c|
-                s += "|"
-                if c.is_alive?
-                    s += "x|"
+        # Populates the next_state var with the proper state
+        def calculate_next_state(neighborhood)
+            #Check states
+            num_live_neighbors = neightborhood.count(true)
+            if this.is_alive?
+                if num_live_neighbors < 2
+                    # Starves
+                    @next_state = false
+
+                elsif num_live_neighbors >= 2 && num_live_neighbors <= 3
+                    # Survives
+                    @next_state = true
 
                 else
-                    s+= " |"
+                    # Suffocates
+                    @next_state = false
+
+                end 
+            else
+                if num_live_neighbors == 3
+                    # Reproduces
+                    @next_state = true
+
+                else
+                    # Stays ded
+                    @next_state = false
 
                 end
             end
         end
-        # Cap it and print
-        s += "-" * (@size * 2 + 1)
-        puts s
 
-    end
+        # Boolean wrappers
+        def is_alive?
+            return @current_state
 
-    # Returns a flattened list of all neighbors of a given location
-    # Location must be a 2-member list of coordinates [row, coloumn]
-    def get_neighborhood(*location)
-        t = @board[location[0]-1].slice(location[1]-1, 3)
-        m = @board[location[0]].slice(location[1]-1, 3)
-        m.delete_at(1)
-        b = @board[location[0]+1].slice(location[1]-1, 3)
-
-        return [t,m,b].flatten
-
-    end
-end
-
-class Cell
-    def initialize
-        @current_state = false
-        @next_state = false
-
-    end
-
-    # Populates the next_state var with the proper state
-    def calculate_next_state(neighborhood)
-        #Check states
-        num_live_neighbors = neightborhood.count(true)
-        if this.is_alive?
-            if num_live_neighbors < 2
-                # Starves
-                @next_state = false
-
-            elsif num_live_neighbors >= 2 && num_live_neighbors <= 3
-                # Survives
-                @next_state = true
-
-            else
-                # Suffocates
-                @next_state = false
-
-            end 
-        else
-            if num_live_neighbors == 3
-                # Reproduces
-                @next_state = true
-
-            else
-                # Stays ded
-                @next_state = false
-
-            end
         end
-    end
 
-    # Boolean wrappers
-    def is_alive?
-        return @current_state
+        def will_be_alive?
+            return @next_state
 
-    end
+        end
 
-    def will_be_alive?
-        return @next_state
+        # State affectors
+        def birth!
+            @current_state = true
 
-    end
+        end
 
-    # State affectors
-    def birth
-        @current_state = true
+        def kill!
+            @current_state = false
 
-    end
-
-    def kill
-        @current_state = false
-
+        end
     end
 end
