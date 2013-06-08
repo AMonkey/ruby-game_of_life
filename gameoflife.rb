ccc19cf24f60318ad3e18930c31dc350418c9e52
Will Jackson <greenerside71@gmail.com>
2013-06-07 18:19:26 -0700

    Super buggy version works, step through in irb. On to a nice, interactive main\!

--- gameoflife.rb
+++ gameoflife.rb
@@ -1,12 +1,34 @@
 #!/usr/bin/ruby
 module GameOfLife
     class Board
+        BORDER = 10
         def initialize(s)
             @size = s
 
             # Makes an array of arrays, each corresponding to a row, filled with dead cells
-            @board = Array.new(@size) { Array.new(@size) { Cell.new } }
+            @board = Array.new(@size+BORDER) { Array.new(@size+BORDER) { Cell.new } }
 
+        end
+
+        # Run one turn of the game
+        def step!
+            # This can be sped up significantly by skipping, which I will do later
+            # Run next-state calculation
+            @size.times do |r|
+                @size.times do |c|
+                    self.get_cell(r,c).set_next_state!(self.get_neighborhood(r,c))
+
+                end
+            end
+
+            # This can also probably be sped up by not going through twice
+            # Evolve!
+            @size.times do |r|
+                @size.times do |c|
+                    self.get_cell(r,c).step!
+
+                end
+            end
         end
 
         def get_cell(r, c)
@@ -22,12 +44,12 @@ module GameOfLife
 
         def print
             s = ''
-            @board.each do |r|
+            @size.times do |r|
                 # Top off each row
                 s << "-" * (@size * 2 + 1)
                 s << "\n|"
-                r.each do |c|
-                    if c.is_alive?
+                @size.times do |c|
+                    if self.get_cell(r,c).is_alive?
                         s << 'x|'
 
                     else
@@ -66,20 +88,23 @@ module GameOfLife
         end
 
         # Populates the next_state var with the proper state
-        def calculate_next_state(neighborhood)
+        def set_next_state!(neighborhood)
             #Check states
-            num_live_neighbors = neightborhood.count(true)
+            num_live_neighbors = neighborhood.count { |x| x.is_alive? }
             if self.is_alive?
                 if num_live_neighbors < 2
                     # Starves
+                    puts "Cell #{self} starves" if $DEBUG
                     @next_state = false
 
                 elsif num_live_neighbors >= 2 && num_live_neighbors <= 3
                     # Survives
+                    puts "Cell #{self} lives" if $DEBUG
                     @next_state = true
 
                 else
                     # Suffocates
+                    puts "Cell #{self} suffocates" if $DEBUG
                     @next_state = false
 
                 end 
@@ -96,12 +121,18 @@ module GameOfLife
             end
         end
 
+        def step!
+            @current_state = @next_state
+
+        end
+
         # Boolean wrappers
         def is_alive?
             return @current_state
 
         end
 
+        # Undefined unless next state has been calculated
         def will_be_alive?
             return @next_state
 
